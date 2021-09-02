e2a2a642630f078b66ac07272e02b8830df149f6
Tina2010 <t.rittmueller@gmx.de>
2021-09-02 12:46:39 +0200

    create script.js
    add first explanations and tests

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+alert('Hello world');
+
+let favoriteFood = 'Chicken';
+document.write(favoriteFood);
+
+/*
+let simpleAddition = 4 + 5;
+document.write(simpleAddition);
+
+let size = 45;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+
+// for pop-ups:
+alert('Hello world');
+
+// "let" creates a variable:
+let age;
+age = 35;
+
+let name;
+name = 'Lisa';
+
+// declaring ageOfLisa to use value of age:
+let ageOfLisa = age;
+ */
+
